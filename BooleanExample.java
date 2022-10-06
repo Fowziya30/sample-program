6050dd240814a7d7636b203da4bd83412df6586a
Fowziya30 <115149789+Fowziya30@users.noreply.github.com>
2022-10-06 17:13:24 +0530

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,13 @@
+package datatypleExample;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean flag=false;
+		//boolean is the datatype
+		//flag is the name
+		//true is the value.
+				System.out.println(flag);
+	}
+
+}
